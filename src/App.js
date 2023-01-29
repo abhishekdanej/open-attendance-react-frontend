cfd54549448513b2014226cfa32da535055603d3
Abhishek Danej <abhi.danej@gmail.com>
2023-01-29 11:09:54 +0000

    attendance retrieval and submission proper

--- src/App.js
+++ src/App.js
@@ -4,30 +4,41 @@ import { useState, useEffect } from "react";
 
 function App() {
 
-  // const [isMailVisible, setIsMailVisible] = useState(true);
-  // const [isButtonVisible, setButtonVisible] = useState(false);
 
-  const [location, setLocation] = useState(null);
   const [pressedButton, setPressedButton] = useState(JSON.parse(localStorage.getItem('pressedButton')) || null);
   const [mail, setMail] = useState(JSON.parse(localStorage.getItem('mail')) || null);
+  // const [mail, setMail] = useState(null);
 
 
   useEffect(() => {
 
     const months = ["Jan", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
     let today = new Date()
-
     const day = today.toString().substring(0, 3)
-
-    let formatted_date = day + "-" + today.getDate() + "-" + months[today.getMonth()] + "-" + today.getFullYear()
+    let formatted_date = day + "-" + today.getDate() + "-" + months[today.getMonth()] + "-" + today.getFullYear();
+    formatted_date = formatted_date.toUpperCase();
 
     var url = "https://iiy5uzcet7.execute-api.ap-south-1.amazonaws.com/dev/attendance?date=" + formatted_date;
 
     // working
     fetch(url)
       .then(response => response.json())
-      .then(result => console.log(result.body))
+      .then((result) => {
+        console.log(result.body)
+        for (const item of result.body) {
+          console.log(item.SK, ' comparing with ', mail, formatted_date);
+
+          if (item.SK === mail && item.PK === formatted_date) {
+            console.log("Attendance is already marked for " + mail);
+            setPressedButton(item.WorkLocation);
+            break;
+          }
+        }
+
+      })
       .catch(error => console.log('error', error));
+
+
 
     //working
     // fetch(urlat )
@@ -43,13 +54,14 @@ function App() {
     //   )
 
 
-  }, [])
+  })
 
 
-  useEffect(() => {
-    localStorage.setItem('mail', JSON.stringify(mail));
-  }, [mail]);
+  // useEffect(() => {
+  //   localStorage.setItem('mail', JSON.stringify(mail));
+  // }, [mail]);
 
+  /*
   useEffect(() => {
     localStorage.setItem('pressedButton', JSON.stringify(pressedButton));
 
@@ -66,7 +78,7 @@ function App() {
       redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    if (mail && pressedButton) {
+    if (mail && pressedButton && false) {
 
       fetch("https://iiy5uzcet7.execute-api.ap-south-1.amazonaws.com/dev/", requestOptions)
         .then(response => response.text())
@@ -77,21 +89,49 @@ function App() {
 
 
   }, [pressedButton]);
+  */
+
+
+  function storeAttendance(payload) {
+
+    setPressedButton(payload);
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({ "mail": mail, "workLocation": payload });
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+
+    console.log("POST to store attendance: ", raw);
+
+    fetch("https://iiy5uzcet7.execute-api.ap-south-1.amazonaws.com/dev/", requestOptions)
+      .then(response => response.text())
+      .then(result => alert(JSON.parse(result).body))
+      .catch(error => console.log('error', error));
+
+  }
 
   function handleMailClick(payload) {
-    // setIsMailVisible(false);
-    // setButtonVisible(true);
 
     setMail(payload);
+    localStorage.setItem('mail', JSON.stringify(payload));
     console.log('App mail: ' + payload);
     // localStorage.setItem('mail', payload);
 
   }
 
   function handleButtonSubmit(payload) {
-    setPressedButton(payload);
+
     console.log("pressed: " + payload);
-    // localStorage.setItem('pressedButton', payload);
+    storeAttendance(payload);
+
   }
 
   return (
